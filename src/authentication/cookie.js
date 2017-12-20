d237c6dca4d1294fa54fd9c25f06ee56c48ffdd5
hasansaad1 <hasansaad.saad1998@gmail.com>
2017-12-20 10:28:46 +0200

    writting the partials and a function to build a cookie reference #8

---
+++ src/authentication/cookie.js
@@ -0,0 +1,17 @@
+const cookie_signature = require("cookie-signature");
+const jwt=require('jsonwebtoken');
+require('env2')('config.env');
+
+
+const create_cookie = user => {
+  let hashedId =  jwt.sign(user.user_id , process.env.SECRET);
+  let cookie = {
+    id: hashedId,
+    name: user.user_name,
+    rights: user.rights
+  };
+  return cookie_signature.sign(JSON.stringify(cookie), process.env.SECRET);
+
+};
+
+module.exports = create_cookie;
